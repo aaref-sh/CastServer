5d861f1603dc0ac015a30a54e9bc9e057f7f589b
aaref-sh <m.aaref-sh@hotmail.com>
2021-07-24 14:23:05 +0300

    create&delete rooms via api - put audio server inside

--- CastServer/Hubs/CastHub.cs
+++ CastServer/Hubs/CastHub.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Threading.Tasks;
-using TCPStreamer;
 
 namespace CastServer.Hubs
 {
@@ -22,16 +23,18 @@ namespace CastServer.Hubs
         public string message;
         public Message(string sender, string message) { this.sender = sender; this.message = message; }
     }
+    public struct room { public string room_name { get; set; } }
     public class CastHub : Hub
     {
         static Dictionary<string, Part[,]> LastScreen = new Dictionary<string, Part[,]>();
         static Dictionary<string, string> groupof = new Dictionary<string, string>();
         static Dictionary<string, List<Message>> messages = new Dictionary<string, List<Message>>();
-        static Dictionary<string, StreamServer> servers = new Dictionary<string, StreamServer>();
+        static Dictionary<string, Process> servers = new Dictionary<string, Process>();
         static Dictionary<string, int> ports = new Dictionary<string, int>();
         static Dictionary<string, string> names = new Dictionary<string, string>();
         public static Dictionary<string, string> sessions = new Dictionary<string, string>();
         string group;
+        static string ipAddress= "192.168.1.111";
 
         static HashSet<string> RoomSet = new HashSet<string>();
         static HashSet<int> PortSet = new HashSet<int>();
@@ -75,7 +78,7 @@ namespace CastServer.Hubs
             messages[group].Add(m);
             await Clients.Group(group).SendAsync("newMessage", name, message);
         }
-        public static bool PortInUse(int port)
+        static bool PortInUse(int port)
         {
             bool inUse = false;
 
@@ -89,21 +92,36 @@ namespace CastServer.Hubs
                 }
             return inUse;
         }
-        public void DeleteRoom(string room_name)
+        public static void DeleteRoom(string room_name)
         {
-            RoomSet.Remove(room_name);
             var group = sessions[room_name];
+            RoomSet.Remove(room_name);
             sessions.Remove(room_name);
             LastScreen.Remove(group);
             messages.Remove(group);
-            servers[group].ConnectToServer();
-            servers[group].disconnect();
-            servers.Remove(group);
+            RemoveServer(group);
             GroupSet.Remove(group);
             PortSet.Remove(ports[group]);
             ports.Remove(group);
+            Console.WriteLine(group + " removed");
         }
-        public bool CreateRoom(string room_name)
+        static void CreateServer(string group,int port)
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+ "\\ConsoleServer.exe";
+            Process p = new Process();
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.FileName = path;
+            p.StartInfo.Arguments = ipAddress + " " + port;
+            p.Start();
+            servers.Add(group,p);
+        }
+        static void RemoveServer(string group)
+        {
+            servers[group].Kill();
+            servers[group].Dispose();
+            servers.Remove(group);
+        }
+        public static bool CreateRoom(string room_name)
         {
             string groupName;
             int newport;
@@ -130,10 +148,7 @@ namespace CastServer.Hubs
                 messages.Add(groupName, new List<Message>());
             }
             catch { }
-            StreamServer streamServer = new StreamServer(newport);
-            servers.Add(groupName, streamServer);
-            streamServer.Init();
-            streamServer.ConnectToServer();
+            CreateServer(groupName, newport);
             return true;
         }
         public async Task getscreen()
@@ -154,7 +169,7 @@ namespace CastServer.Hubs
             Console.WriteLine("OK");
             return "OK";
         }
-        public static string RandomString(int length)
+        static string RandomString(int length)
         {
             var stringChars = new char[length];
             for (int i = 0; i < stringChars.Length; i++)
@@ -168,7 +183,7 @@ namespace CastServer.Hubs
             group = groupName;
         }
 
-        public async Task RemoveFromGroup(string id)
+        async Task RemoveFromGroup(string id)
         {
             string group = groupof[id];
             await Groups.RemoveFromGroupAsync(id, group);
@@ -177,7 +192,6 @@ namespace CastServer.Hubs
         public async void UpdateScreen(string ms, int r, int c, bool encrypted, int height, int width)
         {
             group = groupof[Context.ConnectionId];
-            Console.WriteLine("received " + r + " " + c);
             LastScreen[group][r, c].ms = ms;
             LastScreen[group][r, c].width = width;
             LastScreen[group][r, c].height = height;
